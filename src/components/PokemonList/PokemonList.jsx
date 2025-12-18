04112411a20997e13e9dc9af3d8c56f0b1283df6
dafwa <daffafahir@gmail.com>
2025-12-18 11:34:21 +0800

    add filter state and handleSearch function for searching pokemon names, add styling for PokemonList.css

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -9,6 +9,16 @@ import PokemonItem from "../PokemonItem/PokemonItem";
 function PokemonList() {
     const [pokemons] = useState(pokemonJSON);
 
+    const [filterPokemons, setFilterPokemons] = useState(pokemonJSON);
+
+    const handleSearch = (e) => {
+        let search = pokemons.filter((item) => {
+            return item.name.toLowerCase().includes(e.target.value);
+        });
+
+        setFilterPokemons(search);
+    };
+
     return (
         <div>
             <input
@@ -18,9 +28,11 @@ function PokemonList() {
                 onChange={handleSearch}
             />
             <div className="list-pokemon">
-                {pokemons.map((item) => (
-                    <PokemonItem key={item.id} pokemon={item} />
-                ))}
+                {filterPokemons.length == 0 ? (
+                    <div>data tidak ditemukan</div>
+                ) : (
+                    filterPokemons.map((item) => <PokemonItem key={item.id} pokemon={item} />)
+                )}
             </div>
         </div>
     );
